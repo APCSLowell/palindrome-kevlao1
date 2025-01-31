bd57c420cdb3761297daf2a97dcddcb5d0d1cb37
kevlao1 <kevinlao2k20@gmail.com>
2025-01-30 23:26:38 -0800

    Update PalindromeChecker.java

--- src/main/java/PalindromeChecker.java
+++ src/main/java/PalindromeChecker.java
@@ -37,13 +37,29 @@ public void tester()
 }
 public boolean palindrome(String word)
 {
-  //your code here
+  String word2 = new String("");
+  for(int i = 0; i < word.length(); i++){
+    if(Character.isLetter(word.charAt(i)) == true){
+    word2 = word2 + word.substring(i, i+1);
+  }
+  }
+  if(word2.equalsIgnoreCase(reverse(word))) return true;
   return false;
 }
+
 public String reverse(String str)
 {
-    String sNew = new String();
-    //your code here
+    String sNew1 = new String();
+  for(int i = 0; i < str.length(); i++){
+    if(Character.isLetter(str.charAt(i)) == true){
+      sNew1 = sNew1 + str.substring(i, i+1);
+    }
+  }
+  String sNew = new String();
+   for(int i = 0; i < sNew1.length(); i++){
+    sNew = sNew + sNew1.substring(sNew1.length()-i-1, sNew1.length()-i);
+  }
+  sNew.toLowerCase();
     return sNew;
 }
 }
